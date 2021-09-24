91fc20c20bb720a30a96a77d6658a8ff22c3ce34
Erica Rojo <ericarojo692@yahoo.com>
2021-09-23 19:34:44 -0700

    commit tree 9535f4cce8744f783169dc52c03606520f2ee253

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
 
+    attr_accessor :discount, :items, :last_transaction, :total
+   
+ 
+    def initialization
+        @total = 0 
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity)
+        self.last_transaction 
+
+    end
+
+end
